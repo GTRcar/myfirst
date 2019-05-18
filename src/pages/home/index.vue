8d2f7379de735eba29d93bec2068938beec95712
cuiweipeng <weipeng142@163.com>
2019-05-18 09:52:07 +0800

    完成搜索条

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,16 +1,33 @@
 <template>
-     <div>
-       <div>home</div>
-     </div>
+  <div>
+    <!-- 搜索条 -->
+    <div class="search">
+      <div class="search-item">
+        <icon type="search"/>
+        <span>搜索</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return{}
+    return {};
   }
-}
+};
 </script>
 
 <style  scoped>
+.search {
+  padding: 20rpx;
+  background-color: #eb4450;
+}
+.search .search-item {
+  text-align: center;
+  background-color: #fff;
+}
+.search .search-item icon {
+  vertical-align: middle;
+}
 </style>
