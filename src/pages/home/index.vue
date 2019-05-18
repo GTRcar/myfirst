c1829bd2c248547c084de715b4e60d653881f2ae
cuiweipeng <weipeng142@163.com>
2019-05-18 10:22:21 +0800

    完成轮播图的布局

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,6 +2,11 @@
   <div>
     <!-- 搜索条 -->
     <serach-bar></serach-bar>
+    <swiper :indicator-dots='indicatorDots'>
+    <swiper-item :key="index" v-for="(item,index) in imgUrls">
+      <image :src="item" class="slide-image" />
+    </swiper-item>
+</swiper>
   </div>
 </template>
 
@@ -15,11 +20,21 @@ export default {
     'serach-bar':SearchBar
   },
   data() {
-    return {};
+    return {
+      // 该属性的作用：显示底部的小圆点
+      indicatorDots: true,
+       imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    };
   }
 };
 </script>
 
 <style  scoped>
-
+.slide-image {
+  width: 750rpx;
+}
 </style>
