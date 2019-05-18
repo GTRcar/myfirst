04192302e4cf3e1c19b289718cd97456343b73e1
cuiweipeng <weipeng142@163.com>
2019-05-18 09:37:37 +0800

    完成项目底部菜单配置

---
+++ src/pages/card/index.vue
@@ -0,0 +1,16 @@
+<template>
+     <div>
+         <div>111</div>
+     </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{}
+  }
+}
+</script>
+
+<style  scoped>
+</style>
---
+++ src/pages/my/index.vue
@@ -0,0 +1,16 @@
+<template>
+     <div>
+         <div>333</div>
+     </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{}
+  }
+}
+</script>
+
+<style  scoped>
+</style>
