27d246af8c281b77846c8cea79ebdde9e048101e
cuiweipeng <weipeng142@163.com>
2019-05-20 16:16:27 +0800

    回到顶部完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,13 +2,35 @@
   <div>
     <!-- 搜索条 -->
     <serach-bar></serach-bar>
-    <swiper :indicator-dots="indicatorDots" :autoplay="autoplay" interval=2000>
+    <swiper :indicator-dots="indicatorDots" :autoplay="autoplay" interval="2000">
       <swiper-item :key="index" v-for="(item,index) in imgUrls">
         <image :src="item" class="slide-image"/>
       </swiper-item>
     </swiper>
+    <!-- 菜单导航 -->
     <div class="menu">
-      <img :key='index' v-for='(item, index) in menus' :src="item.image_src">
+      <img :key="index" v-for="(item, index) in menus" :src="item.image_src">
+    </div>
+    <!-- 商品列表布局 -->
+    <div :key='index' v-for='(item, index) in getImgs' class="floor">
+      <div class="floor-title">
+        <img :src="item.floor_title.image_src">
+      </div>
+      <div class="floor-content">
+        <div class="left">
+          <!-- <img :src="item.product_list[0].image_src"> -->
+          <img :src="item.leftImg">
+        </div>
+        <div class="right">
+          <!-- <img v-if='i > 0' :key='i' v-for='(img, i) in item.product_list' :src="img.image_src"> -->
+          <img :key='i' v-for='(img, i) in item.product_list' :src="img.image_src">
+        </div>
+      </div>
+    </div>
+    <!-- 回到顶部 -->
+    <div @click='toTop' v-if='isShow' class="to-top">
+      ︿
+      <p>顶部</p>
     </div>
   </div>
 </template>
@@ -17,8 +39,7 @@
 import SearchBar from "../../components/search";
 import request from "../../utils/request";
 export default {
- async created() {
-    
+  async created() {
     // 调用后台接口，获取轮播数据
     // let that = this;
     // mpvue.request({
@@ -34,20 +55,20 @@ export default {
     //     // console.log(res)
     //   }
     // });
-   
-  //  request('home/swiperdata')
-  //     .then(res => {
-  //       // console.log(res);
-  //       that.imgUrls = res.data.message.map(item => {
-  //         return that.imgUrls = item.image_src
-  //       });
-  //         // console.log(that.imgUrls)
-  //     });
-   let swiper = await request('home/swiperdata');
+
+    //  request('home/swiperdata')
+    //     .then(res => {
+    //       // console.log(res);
+    //       that.imgUrls = res.data.message.map(item => {
+    //         return that.imgUrls = item.image_src
+    //       });
+    //         // console.log(that.imgUrls)
+    //     });
+    let swiper = await request("home/swiperdata");
     let imgs = swiper.data.message.map(item => {
-          return item.image_src;
-        });
-        this.imgUrls = imgs;
+      return item.image_src;
+    });
+    this.imgUrls = imgs;
     // 调用菜单接口
     // mpvue.request({
     //   // 接口地址
@@ -65,10 +86,15 @@ export default {
     //     });
     //     console.log(that.menus)
     //   });
-      let menuRes = await request('home/catitems');
-      this.menus = menuRes.data.message;
-      // console.log(catitems)
-      // console.log(this)
+    let menuRes = await request("home/catitems");
+    this.menus = menuRes.data.message;
+    // console.log(catitems)
+    // console.log(this)
+    // 获取楼层数据
+    let floorRes = await request('home/floordata');
+    // console.log(floorRes);
+    this.floors = floorRes.data.message;
+   
   },
   // 注册组件
   components: {
@@ -80,12 +106,42 @@ export default {
       indicatorDots: true,
       autoplay: true,
       imgUrls: [],
-      menus:[]
+      menus: [],
+      floors:[],
+      isShow:''
     };
   },
-  methods:{
-   
-  }
+  methods: {
+    toTop () {
+      // 控制页面回到顶部
+      mpvue.pageScrollTo({
+        scrollTop: 0
+      });
+    },
+  },
+    onPageScroll (event) {
+     this.isShow = event.scrollTop > 50
+    },
+  // 计算属性
+  computed: {
+    getImgs () {
+      // 加工楼层数据:必须最后返回加工之后的结果
+      // 计算属性依赖于data中的数据
+      return this.floors.map(item => {
+        // 过滤出图片的后四项数据
+        let imgs = item.product_list.filter((img, index)=>{
+          return index > 0
+        });
+        // 添加一条数据保存左侧图片
+        item.leftImg = item.product_list[0].image_src;
+        // 覆盖默认的5张图片
+        item.product_list = imgs;
+        // 把加工之后的数据返回
+        return item;
+      });
+    }
+  },
+
 };
 </script>
 
@@ -100,5 +156,48 @@ export default {
 .menu img {
   width: 128rpx;
   height: 140rpx;
+}
+
+.floor {
+  margin-top: 10rpx;
+}
+.floor .floor-title img {
+  width: 750rpx;
+  height: 80rpx;
+}
+.floor .floor-content {
+  display: flex;
+  justify-content: space-around;
+}
+.floor .floor-content .left img {
+  width: 260rpx;
+  height: 370rpx;
+}
+.floor .floor-content .right {
+  flex: 1;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.floor .floor-content .right img {
+  width:232rpx;
+  height:188rpx;
+  border-radius:4px;
+}
+.to-top {
+  width:100rpx;
+  height:100rpx;
+  border-radius: 50%;
+  background:rgba(255,255,255,0.8);
+  position: fixed;
+  right:40rpx;
+  bottom:40rpx;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.to-top p {
+  font-size: 16px;
 }
 </style>
